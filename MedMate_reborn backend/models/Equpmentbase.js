31aa34fd6649d8c7430f818378409bc8dc45db22
AkumaSingle <Reddish1337@mail.ru>
2024-06-05 21:42:50 +1000

    Добавлены тесты для моделей и исправлены ошибки в существующем коде моделей

--- MedMate_reborn backend/models/Equpmentbase.js
+++ MedMate_reborn backend/models/Equpmentbase.js
@@ -2,11 +2,16 @@ const mongoose = require ('mongoose');
 
  const equipmentbaseSchema = new mongoose.Schema({
     number: {
-        type: number,
+        type: Number,
         required: true,
         unique: true
     },
     docname: {
+        type: String,
         required: true
     }
  })
+
+const EquipmentBase = mongoose.model('EquipmentBase', equipmentbaseSchema);
+
+module.exports = EquipmentBase;
